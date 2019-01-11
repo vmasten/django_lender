f11d19ff9a2cfa3b17c6e6306a306af40fa143cd
Vince Masten <vmasten@me.com>
2019-01-10 23:23:58 -0800

    super broken

--- lender_books/migrations/0002_book_user.py
+++ lender_books/migrations/0002_book_user.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='book',
             name='user',
-            field=models.ForeignKey(default=django.utils.timezone.now, on_delete=django.db.models.deletion.CASCADE, related_name='books', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='books', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
